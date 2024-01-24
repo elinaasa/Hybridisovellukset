d2b5aaf7930d4442d74fd62d1bc66c65e147a536
Elina Sankila <sankilaelina@gmail.com>
2024-01-24 14:19:21 +0200

    get media items by tag

--- Hybridisovellukset/graphql-server/src/api/resolvers/tagResolver.ts
+++ Hybridisovellukset/graphql-server/src/api/resolvers/tagResolver.ts
@@ -1,9 +1,16 @@
-import {fetchAllTags} from '../models/tagModel';
+import {fetchAllTags, fetchTagsByMediaId} from '../models/tagModel';
 
 export default {
-    Query: {
-        tags: async () => {
-            return await fetchAllTags();
-        },
+  Query: {
+    tags: async () => {
+      return await fetchAllTags();
     },
+  },
+  MediaItem: {
+    tags: async (parent: {media_id: string}) => {
+      console.log(parent);
+      const mediaId = Number(parent.media_id);
+      return await fetchTagsByMediaId(mediaId);
+    },
+  },
 };
